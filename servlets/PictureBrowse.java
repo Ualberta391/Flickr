9f9d175c51647d70da5262044966d640b7486cb0
zhanhui913 <zyap@ualberta.ca>
2013-11-17 14:26:43 -0700

    Update PictureBrowse.java

--- servlets/PictureBrowse.java
+++ servlets/PictureBrowse.java
@@ -8,85 +8,112 @@ import java.text.*;
 import java.net.*;
 
 /**
- *  A simple example to demonstrate how to use servlet to 
- *  query and display a list of pictures
- *
- *  @author  Li-Yan Yuan
- *
- */
+* A simple example to demonstrate how to use servlet to
+* query and display a list of pictures
+*
+* @author Li-Yan Yuan
+*
+*/
 public class PictureBrowse extends HttpServlet implements SingleThreadModel {
     
     /**
-     *  Generate and then send an HTML file that displays all the thermonail
-     *  images of the photos.
-     *
-     *  Both the thermonail and images will be generated using another 
-     *  servlet, called GetOnePic, with the photo_id as its query string
-     *
-     */
+* Generate and then send an HTML file that displays all the thermonail
+* images of the photos.
+*
+* Both the thermonail and images will be generated using another
+* servlet, called GetOnePic, with the photo_id as its query string
+*
+*/
     public void doGet(HttpServletRequest request,
-		      HttpServletResponse res)
-	throws ServletException, IOException {
+                 HttpServletResponse response)
+        throws ServletException, IOException {
 
-	//  send out the HTML file
-	res.setContentType("text/html");
-	PrintWriter out = res.getWriter();
+        // send out the HTML file
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
     HttpSession session = request.getSession( true );
 
-	out.println("<html>");
-	out.println("<head>");
-	out.println("<title> Photo List </title>");
-	out.println("</head>");
-	out.println("<body bgcolor=\"#000000\" text=\"#cccccc\" >");
-	out.println("<center>");
-	out.println("<h3>The List of Images </h3>");
+         out.println("<html>");
+        out.println("<head>");
+        out.println("<title> Photo List </title>");
+        out.println("<link rel='stylesheet' type='text/css' href='mystyle.css'>");
+        out.println("</head>");
+        out.println("<body>");
+        
+        out.println("<div id = 'header'>");
+        out.println("<p>&nbsp;</p>");
+        //If there is such attribute as username, this means the user entered this page through
+        //correct navigation (logging in) and is suppose to be here
+        
+        if(request.getSession(false).getAttribute("username") != null){
+            String username = String.valueOf(session.getAttribute("username"));
+            out.println("<p id='username'>You are logged in as "+username+"</p>");
+                    
+            String encode = response.encodeURL("logout.jsp");
+            out.println("<A id='signout' href='"+response.encodeURL (encode)+"'>(Logout)</a>");
+                    
+        }
+        //If user entered this page without logging in or after logging out, redirect user back to main.jsp
+        else{
+            response.sendRedirect("main.jsp");
+        }
+        out.println("</div>");
+        
+        
+        
+        out.println("<div id='container'>");
 
-	/*
-	 *   to execute the given query
-	 */
-	try {
-	    String query = "select photo_id from images";
-	    Connection conn = getConnected();
-	    Statement stmt = conn.createStatement();
-	    ResultSet rset = stmt.executeQuery(query);
-	    String p_id = "";
+        out.println("<center>");
+        out.println("<h3>The List of Images </h3>");
 
-	    while (rset.next() ) {
-	        p_id = (rset.getObject(1)).toString();
-	        // specify the servlet for the image
+        /*
+         * to execute the given query
+         */
+        try {
+         String query = "select photo_id from images";
+         Connection conn = getConnected();
+         Statement stmt = conn.createStatement();
+         ResultSet rset = stmt.executeQuery(query);
+         String p_id = "";
+
+         while (rset.next() ) {
+         p_id = (rset.getObject(1)).toString();
+         // specify the servlet for the image
             out.println("<a href=\"/proj1/DisplayImage.jsp?id="+p_id+"\">");
-	        // specify the servlet for the thumbnail
-	        out.println("<img src=\"/proj1/GetOnePic?"+p_id +
-	                    "\"></a>");
-	    }
-	    stmt.close();
-	    conn.close();
-	} catch ( Exception ex ){ out.println( ex.toString() );}
+         // specify the servlet for the thumbnail
+         out.println("<img src=\"/proj1/GetOnePic?"+p_id +
+         "\"></a>");
+         }
+         stmt.close();
+         conn.close();
+        } catch ( Exception ex ){ out.println( ex.toString() );}
     
-	out.println("<form action='UploadImage.jsp'>");
-	out.println("<input type='submit' value='Add more photos'>");
-	out.println("</form>");
-	out.println("</center>");
-	out.println("</body>");
-	out.println("</html>");
+        out.println("<form action='upload_image.html'>");
+        out.println("<input type='submit' value='Add more photos'>");
+        out.println("</form>");
+        out.println("</center>");
+        out.println("</div>");
+
+        out.println("</body>");
+        out.println("</html>");
     }
     
     /*
-     *   Connect to the specified database
-     */
+* Connect to the specified database
+*/
     private Connection getConnected() throws Exception {
 
-	String username = "vrscott";
-	String password = "radiohead7";
+        String username = "vrscott";
+        String password = "radiohead7";
         /* one may replace the following for the specified database */
 
-	String drivername = "oracle.jdbc.driver.OracleDriver";
-	String dbstring ="jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
-	/*
-	 *  to connect to the database
-	 */
-	Class drvClass = Class.forName(drivername); 
-	DriverManager.registerDriver((Driver) drvClass.newInstance());
-	return( DriverManager.getConnection(dbstring,username,password) );
+        String drivername = "oracle.jdbc.driver.OracleDriver";
+        String dbstring ="jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+        /*
+         * to connect to the database
+         */
+        Class drvClass = Class.forName(drivername);
+        DriverManager.registerDriver((Driver) drvClass.newInstance());
+        return( DriverManager.getConnection(dbstring,username,password) );
     }
 }

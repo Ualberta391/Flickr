d334c5cfe8bf417888a73a60f2a5dac65752201e
Scott Vig <vrscott@ualberta.ca>
2013-11-20 16:19:29 -0700

    Add pic_id sequence to exe.sql

--- sql/exe.sql
+++ sql/exe.sql
@@ -3,5 +3,8 @@
 @drjobdml1.sql;
 @drjobdml2.sql;
 @drjobdml3.sql;
+drop sequence pic_id_sequence;
 drop sequence group_seq;
+create sequence pic_id_sequence;
 create sequence group_seq increment by 1 start with 3;
+
